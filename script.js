12e979603f7569b6007f0a28be90e31b7a4debd8
JACKZINHOPRO <jackzinapela@gmail.com>
2026-02-14 21:12:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const landing = document.getElementById('landing');
+const escolhas = document.getElementById('escolhas');
 
-// Animação de entrada
+// Etapa 1: animação de entrada
 window.onload = () => {
     landing.classList.add('show');
 }
 
-// Função do botão "Entrar" com fade out
+// Botão Entrar
 function entrar() {
     landing.classList.add('fade-out');
     setTimeout(() => {
-        alert("Aqui você pode avançar para a tela das escolhas!");
+        landing.classList.add('hidden');
+        escolhas.classList.remove('hidden');
+        escolhas.classList.add('show');
     }, 500);
+}
+
+// Função para clicar nas escolhas
+function selecionar(tipo) {
+    alert("Você escolheu: " + tipo + " (na próxima etapa você verá as fotos)");
+    // futuramente aqui vamos mostrar a galeria da Etapa 3
 }
