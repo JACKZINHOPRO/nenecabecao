ef69358914f865cff51c29d8ead465baa61f2b0f
JACKZINHOPRO <jackzinapela@gmail.com>
2026-02-14 21:16:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const landing = document.getElementById('landing');
+const entrarBtn = document.getElementById('entrar-btn');
 const escolhas = document.getElementById('escolhas');
+const landingHeader = landing.querySelector('h1');
 
-// Etapa 1: animação de entrada
+// Delay suave do texto e botão na Etapa 1
 window.onload = () => {
-    landing.classList.add('show');
+    setTimeout(() => {
+        landing.classList.add('show'); // adiciona opacidade aos elementos
+    }, 500); // 0.5s de delay antes de aparecer
 }
 
-// Botão Entrar
-function entrar() {
-    landing.classList.add('fade-out');
+// Clique no botão Entrar
+entrarBtn.addEventListener('click', () => {
+    landing.classList.remove('show'); // some a primeira tela
     setTimeout(() => {
-        landing.classList.add('hidden');
-        escolhas.classList.remove('hidden');
-        escolhas.classList.add('show');
-    }, 500);
-}
+        landing.classList.add('hidden'); // esconde de vez
+        escolhas.classList.remove('hidden'); // mostra a tela das escolhas
+    }, 500); // delay para transição suave
+});
 
 // Função para clicar nas escolhas
 function selecionar(tipo) {
-    alert("Você escolheu: " + tipo + " (na próxima etapa você verá as fotos)");
+    alert("Você escolheu: " + tipo);
     // futuramente aqui vamos mostrar a galeria da Etapa 3
 }
